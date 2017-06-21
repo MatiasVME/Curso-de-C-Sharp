8f7e111435cf34beff13d9966005b482cc31e364
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-06-21 15:51:32 -0400

    VT-01

---
+++ VT-01-HolaMundo/Ejemplos/HolaMundo/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!!");
+            Console.Write("Cómo estas?");
+            Console.Write(" Bien y tu?");
+        }
+    }
+}
