b78ffa99b4d920d03de8f9674c80e3687c57c0ca
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-06-23 02:56:15 -0400

    VT-03

---
+++ VT-03-Variables/Ejemplos/Variables/Variables/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            byte edad = 27;
+            Console.WriteLine(edad);
+            edad = 28;
+            Console.WriteLine(edad);
+
+            int numero = 288838;
+            Console.WriteLine(numero);
+
+            bool estaVivo = false;
+            Console.WriteLine(estaVivo);
+
+            string nombre = "Matías";
+            Console.WriteLine(nombre);
+
+            char caracter = 'c';
+            Console.WriteLine(caracter);
+
+            float nota = 5.6f;
+            Console.WriteLine(nota);
+
+            Console.WriteLine("Mi edad es: " + edad);
+            Console.WriteLine("Estoy vivo: " + estaVivo);
+        }
+    }
+}
