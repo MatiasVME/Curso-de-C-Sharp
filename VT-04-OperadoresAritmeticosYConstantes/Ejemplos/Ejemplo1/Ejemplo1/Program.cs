7f4dcf75af983cafd2a0d2cd1979e46c0984b990
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-06-24 21:47:44 -0400

    VT-04

---
+++ VT-04-OperadoresAritmeticosYConstantes/Ejemplos/Ejemplo1/Ejemplo1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const float PI = 3.14f;
+            Console.WriteLine(PI); // hola
+
+            // Este es un comentario
+
+            /* Este es 
+             * un comentario
+             * de multiples lineas
+             */
+
+            int var1 = 5;
+            int var2 = 3;
+            int res = var1 + var2 - (3 + var2) * (var1 - 1);
+
+            Console.WriteLine(res);
+		}
+    }
+}
