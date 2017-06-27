caa3a5cd93691cf1f1dd6623e24a945aa9f09677
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-06-26 20:18:26 -0400

    VT-05

---
+++ VT-05-EntradaDeDatos/Ejemplos/EntradaDeDatos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Application
+{
+    public class EntradaDeDatos
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Cuál es tu nombre?: ");
+            string nombre;
+            nombre = Console.ReadLine();
+            Console.WriteLine("Tu nombre es: " + nombre);
+
+            int edad;
+            Console.WriteLine("Cuál es tu edad?: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tu edad es: " + edad);
+        }
+    }
+}
