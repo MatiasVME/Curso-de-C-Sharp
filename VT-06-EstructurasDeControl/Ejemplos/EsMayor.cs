c2d9bd08eb08becacf7522061be4c1a6b2b0e358
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-06-28 10:46:52 -0400

    VT06

---
+++ VT-06-EstructurasDeControl/Ejemplos/EsMayor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Application
+{
+    public class EsMayor
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ingresa tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad <= 10) {
+                Console.WriteLine("es un niño");
+            }
+
+            else if (edad <= 17) {
+                Console.WriteLine("Es un joven");
+            }
+
+            else {
+                Console.WriteLine("Es un adulto");
+            }
+        }
+    }
+}
---
+++ VT-06-EstructurasDeControl/Ejemplos/EstructurasDeControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Application
+{
+    public class EstructurasDeControl
+    {
+        public static void Main ()
+        {
+            /* > mayor que
+             * < menor que
+             * >= mayor o igual que
+             * <= menor o igual que
+             * == igual que
+             * != distinto que
+             */
+            
+			if (-5 >= 1)
+			{
+                Console.WriteLine("Se hace esto :D");
+
+                if (true != false) {
+                    Console.WriteLine("OK.");
+                }
+			}
+
+            else {
+                Console.WriteLine("si no");
+            }
+
+            Console.WriteLine("Fin.");
+        }
+    }
+}
