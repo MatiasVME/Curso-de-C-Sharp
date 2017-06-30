7622b515db7cbf6a6b71166175f5580e2a42c81f
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-06-30 19:32:01 -0400

    VT-07

---
+++ VT-07-MasSobreEstructurasDeControl/Ejemplos/MasSobreEstructurasDeControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Application
+{
+    public class MasSobreEstructurasDeControl
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            // 5 a 9
+            if (num >= 5 && num <= 9) {
+                Console.WriteLine("Esta dentro del rango!");
+            }
+
+            else {
+                Console.WriteLine("No esta dentro del rango");
+            }
+
+            // Operador condicional AND &&
+            // Operador condicional OR ||
+            // expresion && expresion
+            // expresion || expresion
+        }
+    }
+}
---
+++ VT-07-MasSobreEstructurasDeControl/Ejemplos/OperadorCondicionalTernario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Application
+{
+    public class OperadorCondicionalTernario
+    {
+        public static void Main()
+        {
+            // expresionBoolean ? expression1 : expression2
+
+            Console.WriteLine("Ingrese la hora del día: ");
+            int hora = int.Parse(Console.ReadLine());
+
+            string saludo = "buenos " + ((hora <= 12) ? "dias" : "tardes");
+            Console.WriteLine(saludo);
+        }
+    }
+}
