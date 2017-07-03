7a6f51449aee1f93f61bf91ea09e836bd7619207
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-07-03 16:13:11 -0400

    VT-09

---
+++ VT-09-Ejercicios/Ejemplos/EsPar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Application
+{
+    public class EsPar
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Es par");
+            }
+
+            else {
+                Console.WriteLine("Es impar");
+            }
+        }
+    }
+}
---
+++ VT-09-Ejercicios/Ejemplos/Ordenar3Numeros.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Application
+{
+    public class Ordenar3Numeros
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int mayor = 0, medio = 0, menor = 0;
+
+            if (num1 > num2 && num1 > num3) {
+                mayor = num1;
+
+                if (num2 > num3) {
+                    medio = num2;
+                    menor = num3;
+                }
+
+                else {
+                    medio = num3;
+                    menor = num2;
+                }
+            }
+
+            else if (num2 > num3 && num2 > num1) {
+                mayor = num2;
+
+                if (num1 > num3) {
+                    medio = num1;
+                    menor = num3;
+                }
+
+                else {
+                    medio = num3;
+                    menor = num1;
+                }
+            }
+
+            else if (num3 > num1 && num3 > num2) {
+                mayor = num3;
+
+                if (num1 > num2) {
+                    medio = num1;
+                    menor = num2;
+                }
+
+                else {
+                    medio = num2;
+                    menor = num1;
+                }
+            }
+
+            Console.Write("Resultado: ");
+            Console.WriteLine("{0}, {1}, {2}", menor, medio, mayor);
+        }
+    }
+}
